a0054ae56a22d96a07a248048dad6ff4a608f8d5
gangadhar <gangadhar.b@testyantra.com>
2021-09-06 13:37:28 +0530

    guards

--- Angular/shopping-cart/src/app/app-routing.module.ts
+++ Angular/shopping-cart/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
+import { AuthGuard } from './auth.guard';
 import { DisplayProductComponent } from './display-product/display-product.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-{path:'display',component:DisplayProductComponent},
-{path:'add',component:AddProductComponent},
+{path:'display',component:DisplayProductComponent,data:{roles : ["admin","user"]},
+canActivate:[AuthGuard]},
+{path:'add',component:AddProductComponent,data:{roles : ["admin"]},
+canActivate:[AuthGuard]},
 {path:'register',component:RegisterComponent},
 {path:'login',component:LoginComponent}
 ];
